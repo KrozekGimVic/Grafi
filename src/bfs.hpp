398795812304af8c2a0c809625b70ae03eefdb29
matejm <matejmarinko123@gmail.com>
2016-04-13 17:25:38 +0200

    Added bfs.

---
+++ src/bfs.hpp
@@ -0,0 +1,11 @@
+#ifndef GRAFI_SRC_BFS_HPP
+#define GRAFI_SRC_BFS_HPP
+
+#include "typedefs.hpp"
+
+#include <vector>
+#include <queue>
+
+int count_components_bfs(const std::vector<std::vector<int>>& graph);
+
+#endif //GRAFI_SRC_BFS_HPP
