76a34280b2af3c6c25ff96bbe4ad8dd3afb0669c
matejm <matejmarinko123@gmail.com>
2016-04-13 17:00:04 +0200

    Added gitignore and typedefs, removed build.

--- build/test/googletest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vidd/Desktop/Grafi/test/googletest/googletest
-# Build directory: /home/vidd/Desktop/Grafi/build/test/googletest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
---
+++ src/typedefs.hpp
@@ -0,0 +1,8 @@
+#ifndef GRAFI_SRC_TYPEDEFS_HPP
+#define GRAFI_SRC_TYPEDEFS_HPP
+
+#include <vector>
+
+typedef std::vector<std::vector<int>> graf_t;
+
+#endif //GRAFI_SRC_TYPEDEFS_HPP
