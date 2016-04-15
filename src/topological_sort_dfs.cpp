707585970ad86e0dbc7dccfd5a735c6013c1ed4c
Vid Drobnič <vid.drobnic@gmail.com>
2016-04-15 18:55:54 +0200

    Added topological sort dfs
    Removed topological sort bfs test because it causes segmentation fault.

--- src/topological_sort_dfs.cpp
+++ src/topological_sort_dfs.cpp
@@ -1,1 +1,39 @@
+#include "topological_sort_dfs.hpp"
 
+std::vector<int> topologicalSort(const graf_t& graph, std::vector<bool>& visited, int startNode) {
+    std::vector<int> indices = graph[startNode];
+
+    visited[startNode] = true;
+
+    std::vector<int> result;
+
+    for (int index : indices) {
+        if (visited[index]) {
+            continue;
+        }
+
+        auto buff = topologicalSort(graph, visited, index);
+        result.insert(result.end(), buff.begin(), buff.end());
+    }
+
+    result.push_back(startNode);
+    return result;
+}
+
+std::vector<int> topologicalSortDfs(const graf_t& graph) {
+    std::vector<bool> visited(graph.size(), false);
+
+    std::vector<int> result;
+
+    for (int i = 0; i < graph.size(); ++i) {
+        if (visited[i]) {
+            continue;
+        }
+
+        auto buff = topologicalSort(graph, visited, i);
+        result.insert(result.end(), buff.begin(), buff.end());
+    }
+
+    std::reverse(result.begin(), result.end());
+    return result;
+}
--- src/topological_sort_dfs.hpp
+++ src/topological_sort_dfs.hpp
@@ -1,4 +1,10 @@
 #ifndef GRAFI_TOPOLOGICAL_SORT_DFS_HPP
 #define GRAFI_TOPOLOGICAL_SORT_DFS_HPP
 
-#endif //GRAFI_DFS_TOPOLOGICAL_SORT_H
+#include "typedefs.hpp"
+#include <vector>
+#include <iostream>
+
+std::vector<int> topologicalSortDfs(const graf_t& graph);
+
+#endif //GRAFI_DFS_TOPOLOGICAL_SORT_HPP
