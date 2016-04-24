544fcd4b42706227c805b6944ed73dce706d39b9
Vid Drobnič <vid.drobnic@gmail.com>
2016-04-24 11:49:24 +0200

    Fixed issue #7

---
+++ src/floyd_warshall.cpp
@@ -0,0 +1,28 @@
+#include "floyd_warshall.hpp"
+#include <iostream>
+
+std::vector<std::vector<double>> floyd_warshall(const weighted_graf_t& graph) {
+    const unsigned long N = graph.size();
+    std::vector<std::vector<double>> distance(N, std::vector<double>(N, INFINITY));
+
+    for (int vertex_index = 0; vertex_index < N; ++vertex_index) {
+        distance[vertex_index][vertex_index] = 0.;
+
+        for (const auto& edge : graph[vertex_index]) {
+            distance[vertex_index][edge.first] = edge.second;
+        }
+    }
+
+    for (int k = 0; k < N; ++k) {
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < N; ++j) {
+                if (distance[i][j] > distance[i][k] + distance[k][j]) {
+                    distance[i][j] = distance[i][k] + distance[k][j];
+                }
+            }
+        }
+    }
+
+    return distance;
+
+}
